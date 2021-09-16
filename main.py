ae6a17437260735cc2128fae537e9c842c161dbf
Damian Molinski <tedeer@gmail.com>
2021-09-16 13:44:39 +0200

    This application count vowels and print use words

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 vowels = ['a', 'e', 'i', 'o', 'u'] #vowels ang. samogłoski
 found = []
-word = "Miliard"
+words = []
 
+word = 'none'
+index = 0
 
+while word != "stop":
+    print('Wprowadź słowo: ')
+    word = input()
+    if word != "stop":
+        words.append(word)
+        for letter in word:
+            if letter in vowels:
+                if letter in found:
+                    print("Litera --- " + letter + " --- jest już w naszym zbiorze, więc ją pomijam")
+                else:
+                    found.append(letter)
+        index += 1
 
-for letter in word:
-    if letter in vowels:
-        if letter in found:
-            print("Litera --- " + letter + " --- jest już w naszym zbiorze, więc ją pomijam")
-        else:
-            found.append(letter)
-print(found)
+print('Wykonano ' + str(index) + ' sprawdzeń, a słowa, to:')
+print('Wypisz sprawdzane słowa:')
+for word in words:
+    print(word)
